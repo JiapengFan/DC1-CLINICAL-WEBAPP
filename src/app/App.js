fa603901ca956f142e26c4777097ec548ee74742
AsafKed <asafkedem97@gmail.com>
2020-12-07 10:55:20 +0100

    Create Flask app

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Import components
 import LandingBox from "../uploadPage/LandingBox";
 import FooterBox from "../uploadPage/FooterBox";
 
 const App = () => {
+  const [state, setState] = useState('Hi');
+
+  useEffect(() => {
+    fetch('/hello').then(res => res.json()).then(data => {
+      setState(data.result);
+    });
+  }, []);
+
   return (
     <div className="mainFrame">
       <LandingBox />
+  <p>Flask says {state}</p>
       <FooterBox />
     </div>
   );
